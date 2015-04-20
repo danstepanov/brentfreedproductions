32a2577dbc0fcbb606b6e1471a71625f5b43519c
Dan Stepanov <danhstepanov@gmail.com>
2015-04-20 16:28:03 -0400

    removed unnecessary files

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ jQuery(function($) {'use strict',
 	});
 
 	//backstretch slide for main body
-	$.backstretch([ "images/slide/bg2.png","images/drakebell.png","images/slide/bg3.png"], {fade: 750,duration: 3000});
+	$.backstretch([ "images/slide/bg2.png","images/slide/drakebell.png","images/slide/bg3.png"], {fade: 750,duration: 3000});
 
 	// Feature Tab Content
 	$('.features-nav li').on('click',function(){'use strict',
