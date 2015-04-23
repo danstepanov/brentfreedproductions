72e333b62604c59d163130fc14ae14a6ace66b9d
Dan Stepanov <danhstepanov@gmail.com>
2015-04-23 19:18:10 -0400

    changed sendmail email

--- sendemail.php
+++ sendemail.php
@@ -5,13 +5,13 @@
 		'message'=>'Thank you for contacting us! We will get back to you as early as possible. '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 'bfreed@brentfreedproductions.com';//replace with your email
+    $email_to = 'danhstepanov@gmail.com';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
