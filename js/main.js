191b446735b76ae86a627dc90dfe0287ca32f805
Dan Stepanov <danhstepanov@gmail.com>
2015-08-26 22:46:28 -0700

    removed howie day event and changed background slider to single image per request

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ jQuery(function($) {'use strict',
 	});
 
 	//backstretch slide for main body
-
-	$.backstretch([ "images/slide/howie.jpg","images/slide/aaron.png", "images/slide/bg2.png","images/slide/drakebell.png","images/slide/bg3.png"], {fade: 750,duration: 3000});
+    $.backstretch(["images/slide/drakebell.png"]);
+	//$.backstretch([ "images/slide/howie.jpg","images/slide/aaron.png", "images/slide/bg2.png","images/slide/drakebell.png","images/slide/bg3.png"], {fade: 750,duration: 3000});
 
 	// Feature Tab Content
 	$('.features-nav li').on('click',function(){'use strict',
